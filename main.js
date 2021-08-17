7a9ca539db8e8a5398cc54df4619a126202ef980
anshultamrakar <anshultamrakar487@gmail.com>
2021-08-17 11:14:46 -0400

    syle and js done
    syle and js done

---
+++ main.js
@@ -0,0 +1,40 @@
+const dateOfBirthday = document.querySelector("#birthday");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumber = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box");
+
+
+
+function compareValue(sum , luckyNumber ){
+    if(sum % luckyNumber === 0){
+        outputBox.innerText= "Your number is a lucky Number 😃"
+    }else{
+        outputBox.innerText = "Your number is not a lucky Number 😔"
+    }
+}
+
+
+
+function checkLuckyNumber(){
+    const dob = dateOfBirthday.value ;
+    const sum = calculateSum(dob);
+
+    compareValue(sum ,luckyNumber.value )
+}
+
+
+function calculateSum(dob){
+     dob = dob.replaceAll("-", "");
+    let sum = 0;
+    for(let i = 0 ; i < dob.length ; i++){
+        sum = sum + Number(dob.charAt(i));
+    }
+    return sum ; 
+    
+}
+
+
+
+
+
+checkNumber.addEventListener('click', checkLuckyNumber);
